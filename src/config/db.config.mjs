8ad1c72674eecdee7f4de71d3dcd654c09b44711
DiMouraCg <127412668+DiMouraCg@users.noreply.github.com>
2023-04-09 15:52:28 -0300

    Add files via upload

--- src/config/db.config.mjs
+++ src/config/db.config.mjs
@@ -1,10 +1,10 @@
 import mysql from 'mysql2/promise';
 
 const pool = mysql.createPool({
-  host: '64.31.47.126',
-  user: 'dcontrol_admin',
-  password: '214782Wm#$',
-  database: 'dcontrol_bolaoonline',
+  host: '15.229.6.170',
+  user: 'dimoura',
+  password: '214782214782',
+  database: 'bolao',
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
